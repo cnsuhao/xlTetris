0c6d32cabf43f13a81f80560f17f4cd4a00708c3
Streamlet <streamlet@outlook.com>
2015-04-12 01:13:51 +0800

    optimize shader

--- xlTetris/Res/PixelShader.hlsl
+++ xlTetris/Res/PixelShader.hlsl
@@ -1,32 +1,43 @@
 sampler2D Texture0;
-float2 TexSize;                     // 纹理大小
-int ScanPass;                       // 扫描遍数
-static const int MAX_RADIUS = 50;   // 最大模糊半径
-float Template[MAX_RADIUS];         // 高斯模糊系数，传入一个半径方向上的模糊系数
+float2 TexSize;                                         // 纹理大小
+float ScanPass;                                         // 扫描遍数
+static const int MAX_RADIUS = 200;                      // 最大模糊半径
+static const int MAX_FLOAT4 = (MAX_RADIUS - 1) / 4 + 1;
+float4 Template[MAX_FLOAT4];                            // 高斯模糊系数，传入一个半径方向上的模糊系数
 
 float4 main(float2 texCoord : TEXCOORD0) : COLOR
 {
-    float4 color = tex2D(Texture0, texCoord) * Template[0];
-    float2 coord = 0;
-    bool meetZero = false;
-    for (int i = 1; i < MAX_RADIUS; ++i)
+    float4 coord = texCoord.xyxy;
+    float2 scale = { 1 / TexSize.x, 1 / TexSize.y };
+    float4 color = 0;
+    bool run = true;
+
+    for (int i = 0; i < MAX_FLOAT4; ++i)
     {
-        if (Template[i] != 0 && !meetZero)
+        float4 t = Template[i];
+        if (i == 0)
+            t[0] /= 2;
+        if (run)
         {
-            coord = texCoord;
-            if (ScanPass == 0)
+            for (int j = 0; j < 4; ++j)
             {
-                coord.x = texCoord.x - i / TexSize.x;
-                color += tex2D(Texture0, coord) * Template[i];
-                coord.x = texCoord.x + i / TexSize.x;
-                color += tex2D(Texture0, coord) * Template[i];
-            }
-            else
-            {
-                coord.y = texCoord.y - i / TexSize.y;
-                color += tex2D(Texture0, coord) * Template[i];
-                coord.y = texCoord.y + i / TexSize.y;
-                color += tex2D(Texture0, coord) * Template[i];
+                if (t[j] == 0)
+                    run = false;
+
+                color += tex2D(Texture0, coord.xy) * t[j];
+                color += tex2D(Texture0, coord.zw) * t[j];
+
+                if (ScanPass == 0)
+                {
+                    coord.x -= scale.x;
+                    coord.z += scale.x;
+                }
+                else
+                {
+                    coord.y -= scale.y;
+                    coord.w += scale.y;
+                }
+                coord = clamp(coord, float4(0, 0, 0, 0), float4(1, 1, 1, 1));
             }
         }
     }
